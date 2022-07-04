29b184cd9b1f7e9641a576b41b6cbb9cd94f0eaf
Gaziur Rahman Shishir <gaziur35-3051@diu.edu.bd>
2022-07-04 13:07:03 +0600

    Create a Hotel website

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// header scroll
+let nav = document.querySelector(".navbar");
+window.onscroll= function(){
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+
+    }
+}
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+        
+    })
+})
+
+// swiper
+var swiper= new Swiper(".mySwiper",{
+    direction:"vertical",
+    loop:true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    autoplay:{
+        delay:3500,
+    },
+});
+
