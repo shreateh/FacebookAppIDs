ba12a635c8b93875fdab75434bc4ee9a6aaf3986
Gaziur Rahman Shishir <gaziur35-3051@diu.edu.bd>
2022-08-06 01:18:50 +0600

    service section and counter section are completed

--- js/index.js
+++ js/index.js
@@ -1,34 +1,51 @@
 // header scroll
 let nav = document.querySelector(".navbar");
-window.onscroll= function(){
-    if(document.documentElement.scrollTop > 50){
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
         nav.classList.add("header-scrolled");
-    }
-    else{
+    } else {
         nav.classList.remove("header-scrolled");
-
     }
-}
+};
 
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(a){
-    a.addEventListener("click",function(){
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
         navCollapse.classList.remove("show");
-        
-    })
-})
+    });
+});
 
 // swiper
-var swiper= new Swiper(".mySwiper",{
-    direction:"vertical",
-    loop:true,
-    pagination:{
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
         el: ".swiper-pagination",
-        clickable:true,
+        clickable: true,
     },
-    autoplay:{
-        delay:3500,
+    autoplay: {
+        delay: 3500,
     },
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 1000, 3000);
+    counter("count2", 100, 1110, 3000);
+    counter("count3", 0, 1560, 3000);
+    counter("count4", 0, 1240, 3000);
+});
